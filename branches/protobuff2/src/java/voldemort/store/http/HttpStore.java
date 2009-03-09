ef4eebb7d1a7a3a3a7a956c4085fb1c3437b52fc
jay.kreps <jay.kreps@9b603dc2-d86a-11dd-9aad-3f7dfcff7c77>
2009-03-09 06:38:50 +0000

    Create new protobuff branch to catch up on all the changes.

---
+++ branches/protobuff2/src/java/voldemort/store/http/HttpStore.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2008-2009 LinkedIn, Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package voldemort.store.http;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.DeleteMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+
+import voldemort.VoldemortException;
+import voldemort.store.Store;
+import voldemort.store.StoreUtils;
+import voldemort.store.UnreachableStoreException;
+import voldemort.utils.ByteArray;
+import voldemort.utils.ByteUtils;
+import voldemort.versioning.VectorClock;
+import voldemort.versioning.Version;
+import voldemort.versioning.Versioned;
+
+/**
+ * A remote store client that transmits operations via HTTP and interacts with
+ * the VoldemortHttpServer.
+ * 
+ * @author jay
+ */
+public class HttpStore implements Store<ByteArray, byte[]> {
+
+    private static final Hex codec = new Hex();
+    private static final HttpResponseCodeErrorMapper httpResponseCodeErrorMapper = new HttpResponseCodeErrorMapper();
+    private static final String VERSION_EXTENSION = "X-vldmt-version";
+
+    private final String storeName;
+    private final String host;
+    private final int port;
+    private final HttpClient httpClient;
+
+    public HttpStore(String storeName, String host, int port, HttpClient client) {
+        this.storeName = storeName;
+        this.host = host;
+        this.port = port;
+        this.httpClient = client;
+    }
+
+    public boolean delete(ByteArray key, Version version) throws VoldemortException {
+        StoreUtils.assertValidKey(key);
+        String url = getUrl(key);
+        DeleteMethod method = null;
+        try {
+            method = new DeleteMethod(url);
+            VectorClock clock = (VectorClock) version;
+            method.setRequestHeader(VERSION_EXTENSION,
+                                    new String(Base64.encodeBase64(clock.toBytes())));
+            int response = httpClient.executeMethod(method);
+            if(response == HttpURLConnection.HTTP_NOT_FOUND)
+                return false;
+            if(response != HttpURLConnection.HTTP_OK)
+                httpResponseCodeErrorMapper.throwError(response, method.getStatusText());
+            return true;
+        } catch(HttpException e) {
+            throw new VoldemortException(e);
+        } catch(IOException e) {
+            throw new UnreachableStoreException("Could not connect to " + url + " for " + storeName,
+                                                e);
+        } finally {
+            if(method != null)
+                method.releaseConnection();
+        }
+    }
+
+    public List<Versioned<byte[]>> get(ByteArray key) throws VoldemortException {
+        StoreUtils.assertValidKey(key);
+        String url = getUrl(key);
+        GetMethod method = null;
+        try {
+            method = new GetMethod(url);
+            int response = httpClient.executeMethod(method);
+            if(response != HttpURLConnection.HTTP_OK)
+                httpResponseCodeErrorMapper.throwError(response, method.getStatusText());
+            DataInputStream input = new DataInputStream(new ByteArrayInputStream(method.getResponseBody()));
+            List<Versioned<byte[]>> items = new ArrayList<Versioned<byte[]>>();
+            try {
+                while(true) {
+                    int size = input.readInt();
+                    byte[] bytes = new byte[size];
+                    ByteUtils.read(input, bytes);
+                    VectorClock clock = new VectorClock(bytes);
+                    byte[] data = ByteUtils.copy(bytes, clock.sizeInBytes(), bytes.length);
+                    items.add(new Versioned<byte[]>(data, clock));
+                }
+            } catch(EOFException e) {
+                input.close();
+            }
+            return items;
+        } catch(HttpException e) {
+            throw new VoldemortException(e);
+        } catch(IOException e) {
+            throw new UnreachableStoreException("Could not connect to " + url + " for " + storeName,
+                                                e);
+        } finally {
+            if(method != null)
+                method.releaseConnection();
+        }
+    }
+
+    public Map<ByteArray, List<Versioned<byte[]>>> getAll(Iterable<ByteArray> keys)
+            throws VoldemortException {
+        StoreUtils.assertValidKeys(keys);
+        // TODO Consider retrieving the keys concurrently.
+        return StoreUtils.getAll(this, keys);
+    }
+
+    public void put(ByteArray key, Versioned<byte[]> versioned) throws VoldemortException {
+        StoreUtils.assertValidKey(key);
+        String url = getUrl(key);
+        PutMethod method = null;
+        try {
+            method = new PutMethod(url);
+            VectorClock clock = (VectorClock) versioned.getVersion();
+            method.setRequestHeader(VERSION_EXTENSION,
+                                    new String(Base64.encodeBase64(clock.toBytes()), "UTF-8"));
+            method.setRequestEntity(new ByteArrayRequestEntity(versioned.getValue()));
+            int response = httpClient.executeMethod(method);
+            if(response != HttpURLConnection.HTTP_OK)
+                httpResponseCodeErrorMapper.throwError(response, method.getStatusText());
+        } catch(HttpException e) {
+            throw new VoldemortException(e);
+        } catch(IOException e) {
+            throw new UnreachableStoreException("Could not connect to " + url + " for " + storeName,
+                                                e);
+        } finally {
+            if(method != null)
+                method.releaseConnection();
+        }
+    }
+
+    public void close() {}
+
+    public String getName() {
+        return storeName;
+    }
+
+    private String getUrl(ByteArray key) throws VoldemortException {
+        return "http://" + host + ":" + port + "/" + getName() + "/"
+               + ByteUtils.getString(codec.encode(key.get()), "UTF-8");
+    }
+
+}
---
+++ branches/protobuff2/src/java/voldemort/store/metadata/MetadataStore.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2008-2009 LinkedIn, Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package voldemort.store.metadata;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+
+import voldemort.VoldemortException;
+import voldemort.cluster.Cluster;
+import voldemort.store.Entry;
+import voldemort.store.StorageEngine;
+import voldemort.store.Store;
+import voldemort.store.StoreDefinition;
+import voldemort.store.StoreUtils;
+import voldemort.utils.ByteArray;
+import voldemort.utils.ByteUtils;
+import voldemort.utils.ClosableIterator;
+import voldemort.utils.Utils;
+import voldemort.versioning.Version;
+import voldemort.versioning.Versioned;
+import voldemort.xml.ClusterMapper;
+import voldemort.xml.StoreDefinitionsMapper;
+
+import com.google.common.collect.ImmutableSet;
+
+public class MetadataStore implements StorageEngine<ByteArray, byte[]> {
+
+    public static final String METADATA_STORE_NAME = "metadata";
+    public static final String CLUSTER_KEY = "cluster.xml";
+    public static final String STORES_KEY = "stores.xml";
+    public static final Set<String> KNOWN_KEYS = ImmutableSet.of("cluster.xml", "stores.xml");
+
+    private final File directory;
+    private final ClusterMapper clusterMapper;
+    private final StoreDefinitionsMapper storeMapper;
+    private final Map<String, ? extends Store<ByteArray, byte[]>> stores;
+
+    public MetadataStore(File directory, Map<String, ? extends Store<ByteArray, byte[]>> stores) {
+        this.directory = directory;
+        this.storeMapper = new StoreDefinitionsMapper();
+        this.clusterMapper = new ClusterMapper();
+        this.stores = stores;
+        if(this.directory.listFiles() == null)
+            throw new IllegalArgumentException("No configuration found in "
+                                               + this.directory.getAbsolutePath() + ".");
+        if(!this.directory.exists() && this.directory.canRead())
+            throw new IllegalArgumentException("Metadata directory "
+                                               + this.directory.getAbsolutePath()
+                                               + " does not exist or can not be read.");
+    }
+
+    public String getName() {
+        return METADATA_STORE_NAME;
+    }
+
+    public boolean delete(ByteArray key, Version version) throws VoldemortException {
+        throw new VoldemortException("You can't delete your metadata, fool!");
+    }
+
+    /**
+     * Store the new metadata, and apply any changes made by adding or deleting
+     * stores
+     */
+    public void put(ByteArray key, Versioned<byte[]> value) throws VoldemortException {
+        throw new VoldemortException("No metadata modifications allowed (yet).");
+    }
+
+    public void close() throws VoldemortException {
+
+    }
+
+    public List<Versioned<byte[]>> get(ByteArray key) throws VoldemortException {
+        String keyStr = new String(key.get());
+        if(!KNOWN_KEYS.contains(keyStr))
+            throw new IllegalArgumentException("Unknown metadata key: " + keyStr);
+        File file = new File(this.directory, keyStr);
+        if(!Utils.isReadableFile(file.getAbsolutePath()))
+            throw new VoldemortException("Attempt to read metadata failed: "
+                                         + file.getAbsolutePath() + " is not a readable file!");
+        try {
+            return Collections.singletonList(new Versioned<byte[]>(FileUtils.readFileToByteArray(file)));
+        } catch(IOException e) {
+            throw new VoldemortException("Error reading metadata value '" + keyStr + "': ", e);
+        }
+    }
+
+    public Map<ByteArray, List<Versioned<byte[]>>> getAll(Iterable<ByteArray> keys)
+            throws VoldemortException {
+        StoreUtils.assertValidKeys(keys);
+        return StoreUtils.getAll(this, keys);
+    }
+
+    public Cluster getCluster() {
+        return clusterMapper.readCluster(createStringReader(CLUSTER_KEY));
+    }
+
+    private StringReader createStringReader(String keyName) {
+        return new StringReader(getSingleValue(get(new ByteArray(ByteUtils.getBytes(keyName,
+                                                                                    "UTF-8")))));
+    }
+
+    public List<StoreDefinition> getStores() {
+        return storeMapper.readStoreList(createStringReader(STORES_KEY));
+    }
+
+    private String getSingleValue(List<Versioned<byte[]>> found) {
+        if(found.size() != 1)
+            throw new VoldemortException("Inconsistent metadata found: expected 1 version but found "
+                                         + found.size());
+        return ByteUtils.getString(found.get(0).getValue(), "UTF-8");
+    }
+
+    public ClosableIterator<Entry<ByteArray, Versioned<byte[]>>> entries() {
+        throw new IllegalStateException("Not implemented.");
+    }
+
+}
